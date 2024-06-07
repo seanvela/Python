02cee481718a5fee7e40594c6fe924f352f67f32
Sean Vela <sean_vela@yahoo.com>
2024-06-07 11:04:35 -0500

    changes to App1

--- exercises/e6.py
+++ exercises/e6.py
@@ -1,15 +1,24 @@
 import FreeSimpleGUI as sg
+from converter14 import convert
 
 label1 = sg.Text('Enter Feet: ')
-input1 = sg.Input()
+input1 = sg.Input(key='feet')
 label2 = sg.Text('Enter inches: ')
-input2 = sg.Input()
+input2 = sg.Input(key='inches')
 convert_button = sg.Button('Convert')
+output_label = sg.Text('', key='output')
 
 window = sg.Window('Converter', layout=[[label1, input1], 
                                               [label2, input2],
-                                              [convert_button]])
+                                              [convert_button, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    feet = float(values['feet'])
+    inches = float(values['inches'])
+
+    result = convert(feet, inches)
+    window['output'].update(value=f"{result} m", text_color = 'yellow')
+
 window.close()
 
